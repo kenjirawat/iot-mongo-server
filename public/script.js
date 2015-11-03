fe84f9faa860abf2695380d20bee01741f52ae23
kenjirawat <jirawat_ken@hotmail.com>
2015-11-03 15:43:40 +0700

    move Chart

--- public/script.js
+++ public/script.js
@@ -27,4 +27,53 @@ angular.module('app', [])
           alert(response.data.message)
         })
     }
+ //////////// Chart ///////////
+    var data1 = [
+            {
+                value: 300,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 50,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            },
+            {
+                value: 100,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ]
+        
+        var data2 = [
+            {
+                value: 60,
+                color:"#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 32,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            }
+        ]
+
+        var ctx1 = document.getElementById("myChart").getContext("2d");
+        new Chart(ctx1).Doughnut(data1, {
+            animateScale: true
+        });
+
+        // new Chart(ctx).Radar(data);
+        var ctx2 = document.getElementById("doung").getContext("2d");
+        new Chart(ctx2).Doughnut(data2, {
+            animateScale: true
+        });
+
+
   })
